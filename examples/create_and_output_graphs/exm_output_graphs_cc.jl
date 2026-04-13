8d77c941ab5ca7d282fcda3a1bd1fb3af56735b4
Yurii-Nosov <y.nosov.math@gmail.com>
2026-04-13 15:33:11 +0300

    Create exm_output_graphs_cc.jl
    create exm_output_graphs_cc.jl

---
+++ examples/create_and_output_graphs/exm_output_graphs_cc.jl
@@ -0,0 +1,35 @@
+using EnumerateLinearPolyenes
+
+#  Example of displaying images of molecular graphs with depleted hydrogen
+#  atoms based on a list of vertex codes
+
+
+
+#  Generating a list of vertex codes for molecular graphs of isomers
+
+n = 8 #   #  Order of molecular graphs.
+ctype = 1  #  Vertex code used
+
+list_vertex_code = generate_isomer_vertex(n)
+
+# Displaying images of molecular graphs of isomers
+dirpath = "C:Julia_results\\Figures\\Isomers\\"
+
+if !isdir(dirpath)
+    mkpath(dirpath)
+end
+
+print_list_graphs_cc(list_vertex_code, dirpath)
+# --------------------------------------------------------
+# Generating a list of vertex codes for molecular graphs of trans-isomer conformers
+
+list_vertex_code = generate_trans_conformer_edge(n)
+
+# Displaying images of molecular graphs of trans-isomer conformers
+dirpath = "C:Julia_results\\Figures\\Conformers\\"
+
+if !isdir(dirpath)
+    mkpath(dirpath)
+end
+
+print_list_graphs_cc(list_vertex_code, dirpath)
